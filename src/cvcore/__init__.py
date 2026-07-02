6746d2be794f2634564bd7af9992f57b6848a547
gracy-rm <yadavgracy4@gmail.com>
2026-07-02 19:52:21 +0530

    feat: restructure library layout into functional package cvcore

---
+++ src/cvcore/__init__.py
@@ -0,0 +1,5 @@
+from .image_operations.io import load_image, save_image, image_info, convert_color
+from .image_operations.geometric import crop, resize, rotate, translate, flip, pad
+from .image_operations.enhancement import blur, roi_blur, detect_edge, blend
+
+from .plot_operations.plot import image_row, image_grid, bbox, segment
